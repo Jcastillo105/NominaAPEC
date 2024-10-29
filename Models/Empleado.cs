174abe7629b8e1b77673932403a5699b031d65c1
Jose Eduardo Castillo Soriano <j.castillo105@unapec.edu.do>
2024-10-28 21:00:58 -0400

    verificacion de cedula agregada

--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NominaAPEC.Models
 {
@@ -8,6 +9,8 @@ namespace NominaAPEC.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Cédula")]
+        [CedulaValidation(ErrorMessage = "La cédula ingresada no es válida.")]
         public string Cedula { get; set; }
 
         [Required]
@@ -18,8 +21,48 @@ namespace NominaAPEC.Models
         public string Puesto { get; set; }
 
         [Required]
+        [Display(Name = "Salario Mensual")]
+        [Range(0, double.MaxValue, ErrorMessage = "El salario mensual debe ser un número positivo.")]
         public decimal SalarioMensual { get; set; }
 
         public int NominaId { get; set; }
+    }
+
+    // Agregar una clase de validación personalizada para la cédula
+    public class CedulaValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && ValidaCedula(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+
+        private bool ValidaCedula(string pCedula)
+        {
+            int vnTotal = 0;
+            string vcCedula = pCedula.Replace("-", "");
+            int pLongCed = vcCedula.Trim().Length;
+            int[] digitoMult = new int[11] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+
+            if (pLongCed != 11)
+                return false;
+
+            for (int vDig = 1; vDig <= pLongCed; vDig++)
+            {
+                int vCalculo = Int32.Parse(vcCedula.Substring(vDig - 1, 1)) * digitoMult[vDig - 1];
+                if (vCalculo < 10)
+                    vnTotal += vCalculo;
+                else
+                    vnTotal += Int32.Parse(vCalculo.ToString().Substring(0, 1)) + Int32.Parse(vCalculo.ToString().Substring(1, 1));
+            }
+
+            return vnTotal % 10 == 0;
+        }
     }
 }
